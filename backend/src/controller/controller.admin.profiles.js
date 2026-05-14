683e02bac5278016c49ae1011f889b9ae4e6bbc9
Lungu Marian <141348340+Marian097@users.noreply.github.com>
2026-05-14 11:29:08 +0300

    am facut functii de aprobare, respingere

--- backend/src/controller/controller.admin.profiles.js
+++ backend/src/controller/controller.admin.profiles.js
@@ -1,11 +1,34 @@
 import { pool } from "../db.js";
 
 
-export async function getProfiles(req, res, next)
+export async function getProfilesUnverified(req, res, next)
 {
   const db = await pool.connect();
   try{
-    const results = await db.query("SELECT wp.id, wp.full_name AS Nume, wp.phone AS telefon, u.email AS email, c.name AS calificare, wp.created_at AS data_inregistrare, wp.updated_at AS data_actualizare FROM users u JOIN worker_profiles wp ON u.id = wp.user_id JOIN worker_categories wc ON wp.id = wc.worker_profile_id JOIN categories c ON wc.category_id = c.id")
+    const results = await db.query("SELECT wp.id, wp.full_name AS Nume, wp.phone AS telefon, u.email AS email, c.name AS calificare, wp.created_at AS data_inregistrare, wp.updated_at AS data_actualizare, wp.is_verified AS verified, wp.is_approved AS approved FROM users u JOIN worker_profiles wp ON u.id = wp.user_id JOIN worker_categories wc ON wp.id = wc.worker_profile_id JOIN categories c ON wc.category_id = c.id WHERE wp.is_verified = $1 OR wp.is_approved = $2", [false, false])
+
+    if (results.rows.length === 0)
+    {
+      return res.status(404).json({message: "Nici un rezultat"})
+    }
+
+    return res.status(200).json(results.rows)
+  }
+  catch(err){
+    return res.status(500).json({message: err.message})
+  }
+  finally{
+    db.release()
+  }
+}
+
+
+
+export async function getProfilesVerified(req, res, next)
+{
+  const db = await pool.connect();
+  try{
+    const results = await db.query("SELECT wp.id, wp.full_name AS Nume, wp.phone AS telefon, u.email AS email, c.name AS calificare, wp.created_at AS data_inregistrare, wp.updated_at AS data_actualizare, wp.is_verified AS verified, wp.is_approved AS approved FROM users u JOIN worker_profiles wp ON u.id = wp.user_id JOIN worker_categories wc ON wp.id = wc.worker_profile_id JOIN categories c ON wc.category_id = c.id WHERE wp.is_verified = $1 OR wp.is_approved = $2", [true, true])
 
     if (results.rows.length === 0)
     {
@@ -22,16 +45,16 @@ export async function getProfiles(req, res, next)
   }
 }
  
+ 
 export async function hasProfileApproved(req, res, next)
 {
   const db = await pool.connect()
   const user_id = req.user.id;
-  const { id, is_verified, is_approved, is_featured} = req.body;
+  const { id } = req.body;
 
   try{
     const result = await db.query("SELECT ur.role_id, r.role_name AS role FROM user_roles ur JOIN roles r ON ur.role_id = r.id WHERE user_id = $1", [user_id])
 
-    console.log("Dupa aflarea rolului")
 
     if (result.rows.length === 0)
     {
@@ -51,7 +74,58 @@ export async function hasProfileApproved(req, res, next)
 
     await db.query("BEGIN")
 
-    const update_profile = await db.query("UPDATE worker_profiles SET is_verified = $1, is_approved = $2, is_featured = $3 WHERE id = $4 RETURNING *", [is_verified, is_approved, is_featured, id])
+    const update_profile = await db.query("UPDATE worker_profiles SET is_verified = $1, is_approved = $2 WHERE id = $4 RETURNING *", [true, true, id])
+
+    console.log(update_profile.rows)
+
+    if (update_profile.rows.length === 0)
+    {
+      await db.query("ROLLBACK");
+      return res.status(500).json({message: "Operatiunea a esuat"})
+    }
+
+    await db.query("COMMIT");
+    return res.status(200).json({message: "Aprobare cu succes"})
+  }
+  catch(err){
+    await db.query("ROLLBACK");
+    return res.status(500).json({message: err.message})
+  }
+  finally{
+    db.release()
+  }
+}
+
+
+export async function hasProfileRejected(req, res, next)
+{
+  const db = await pool.connect()
+  const user_id = req.user.id;
+  const { id } = req.body;
+
+  try{
+    const result = await db.query("SELECT ur.role_id, r.role_name AS role FROM user_roles ur JOIN roles r ON ur.role_id = r.id WHERE user_id = $1", [user_id])
+
+
+    if (result.rows.length === 0)
+    {
+      return res.status(404).json({message: "Userul nu a fost gasit."})
+    }
+
+    const role_name = result.rows[0].role;
+
+    console.log("Rol:", role_name)
+   
+    if (role_name !== "Admin")
+    {
+      return res.status(403).json({message: "Nu puteti face aceste modficari"})
+    }
+
+    console.log("Dupa verificarea rolului")
+
+    await db.query("BEGIN")
+
+    const update_profile = await db.query("UPDATE worker_profiles SET is_verified = $1, is_approved = $2 WHERE id = $4 RETURNING *", [false, false, id])
 
     console.log(update_profile.rows)
 
--- backend/src/routes/adminRouter.js
+++ backend/src/routes/adminRouter.js
@@ -2,14 +2,19 @@ import { Router } from "express";
 
 import { verifiedToken } from "../middleware/jwt.middleware.js";
 
-import { hasVerifiedUserDocuments } from "../controller/controller.admin.documents.js";
-import { getProfiles, hasProfileApproved } from "../controller/controller.admin.profiles.js";
+import { hasVerifiedAcceptUserDocuments, hasVerifiedRejectedUserDocuments, getUserDocumentsUnverified, getUserDocumentsVerified } from "../controller/controller.admin.documents.js";
+import { getProfilesVerified, getProfilesUnverified, hasProfileApproved, hasProfileRejected } from "../controller/controller.admin.profiles.js";
 import { deleteUser, findAllUsers } from "../controller/controller.admin.user.js";
 
 export const adminRouter = Router();
 
-adminRouter.put("/admin/documents/verify", verifiedToken, hasVerifiedUserDocuments);
-adminRouter.get("/admin/profiles", getProfiles);
-adminRouter.put("/admin/profile/verify", verifiedToken, hasProfileApproved );
+adminRouter.update("/admin/documents/verify/accept", verifiedToken, hasVerifiedAcceptUserDocuments);
+adminRouter.update("/admin/documents/verify/reject", verifiedToken, hasVerifiedRejectedUserDocuments);
+adminRouter.get("/admin/documents/unverified", verifiedToken, getUserDocumentsUnverified);
+adminRouter.get("/admin/documents/verified", verifiedToken, getUserDocumentsVerified);
+adminRouter.get("/admin/profiles/verified", getProfilesVerified);
+adminRouter.get("/admin/profiles/unverified", getProfilesUnverified);
+adminRouter.put("/admin/profile/verify/accept", verifiedToken, hasProfileApproved );
+adminRouter.put("/admin/profile/verify/reject", verifiedToken, hasProfileRejected );
 adminRouter.delete("/admin/user", deleteUser);
 adminRouter.get("/admin/user", findAllUsers );
